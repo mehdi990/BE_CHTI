3bef96d87558a7109fb115b1f01eb597a7c524bd
mehdi <kigoite@gmail.com>
2022-04-04 10:55:32 +0200

    on a fini la question 4

--- Src/GestionSon.s
+++ Src/GestionSon.s
@@ -1,16 +1,21 @@
 	PRESERVE8
 	THUMB   
-	#include "GestionSon.h"  	
+	INCLUDE DriverJeuLaser.inc  	
 
 ; ====================== zone de réservation de données,  ======================================
+	import Son
+	import LongueurSon
+	export CallbackSon
+	export SortieSon
 ;Section RAM (read only) :
 	area    mesdata,data,readonly
 
 
 ;Section RAM (read write):
 	area    maram,data,readwrite
-		
 
+index dcd 0
+SortieSon dcw 0
 	
 ; ===============================================================================================
 	
@@ -19,25 +24,47 @@
 		
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
+		
+
 ; écrire le code ici
 CallbackSon proc
 ;
 ;if(indice<long)
-	push {r4-r6,lr}
-	ldr r0,=son
+	push {r4,r5,r6,r7,r8,lr}
+	ldr r0,=Son
 	ldr r2,=index
+	ldr r6,[r2]
 	ldr r4,=LongueurSon
-	ldrsh r1,[r0,r2,lsl#1]
-	cmp r2,r4
-	bge end
-	add r2,r2,#1
-	mov r5,#719
-	
-	
-	
+	ldr r8,=SortieSon
+	;ldr r3, [r2]
+	ldr r7, [r4]
 
+	cmp r6,r7
+	bge fin  ; fin de if
+	ldrsh r1,[r0,r6,lsl#1]
+
+		
 ;Rx= (son[indice]+32768)/92)
-;indice++
+	mov r3,#0
+	mov r5,#719
+
+	add r3,#32768
+	mul r5,r5,r3
+	lsr r5,r5,#16 ; r5/2^16
+	str r5,[r0]
+	
+	str r5,[r8]
+;indice++	
+
+	add r6,r6,#1
+	str r6,[r2]
+	pop {r4,r5,r6,r7,lr}
+	
+fin
+	bx lr
+
+
+
 ;
 ;
 ;		
@@ -46,6 +73,6 @@ CallbackSon proc
 
 
 
-
+endp
 
 END	
