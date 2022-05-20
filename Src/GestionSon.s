59a61f44e489b59402ebabff959e92e85f81d245
mehdi <kigoite@gmail.com>
2022-05-20 18:27:31 +0200

    on a ajouté le calcul de la dft sur 64 freq, il reste qu'a tester la partie fusion du son et à gérer l'affichage des scores

---
+++ Src/GestionSon.s
@@ -0,0 +1,119 @@
+	PRESERVE8
+	THUMB   
+	import Son	
+	import LongueurSon
+	export CallbackSon
+	export SortieSon	
+	include DriverJeuLaser.inc  	
+
+; ====================== zone de réservation de données,  ======================================
+	
+	
+	
+;Section RAM (read only) :
+	area    mesdata,data,readonly
+
+
+;Section RAM (read write):
+	area    maram,data,readwrite
+
+index dcd 0
+SortieSon dcd 0
+	
+; ===============================================================================================
+	
+
+
+		
+;Section ROM code (read only) :		
+	area    moncode,code,readonly
+		
+
+; écrire le code ici
+StartSon proc
+	ldr r0, =index
+	mov r1, #0
+	str r1, [r0]
+	ENDP
+
+
+
+CallbackSon proc
+;
+;if(indice<long)
+	push {r4,r5,r6,r7,r8,lr}
+	ldr r4,=LongueurSon 
+	ldr r7, [r4] ;r7--> val de longueurS                     
+	ldr r2,=index
+	ldr r6,[r2]
+	cmp r6,r7
+	bgt boucle
+	;if index>LongueurS move to continue
+	ldr r0,=Son
+	ldrsh r1,[r0,r6,lsl#1]
+	
+	
+	ldr r8,=SortieSon
+	;ldr r3, [r2]
+	add r1,#32768
+	mov r5,#719
+	mul r1,r1,r5
+	asr r1, #16
+	str r1,[r8]
+	mov r0,r1
+	push{r2}
+	bl PWM_Set_Value_TIM3_Ch3
+	pop{r2}
+;continue
+    ;indice++	
+
+	add r6,r6,#1
+	str r6,[r2]
+    b fin
+boucle
+	push {lr}
+    bl StartSon
+	pop{lr}
+	
+fin	
+	pop {r4,r5,r6,r7,r8,lr}
+	bx lr
+	
+	
+	
+	
+	ENDP
+	END
+	
+
+	
+	;bge fin  ; fin de if
+	
+
+		
+;Rx= (son[indice]+32768)/92)
+	;mov r3,#0
+	
+
+	
+	
+	;lsr r5,r5,#16 ; r5/2^16
+	
+	
+	;str r5,[r8]
+	
+
+	
+	
+
+
+
+
+;
+;
+;		
+;		
+
+
+
+
--- Src/principal.c
+++ Src/principal.c
@@ -1,12 +1,68 @@
 
 
 #include "DriverJeuLaser.h"
+#include "GestionSon.h"
+
 
 extern int THEDFT(short int* signal64ech, char k);
 extern short LeSignal[64];
+extern short int TabCos;
+extern short int TabSin;
+int compt[4];
+
+
 int resultat[64];
+short int dma_buf[64];
 
+void StartSon(void);
 
+void CallbackSon(void);
+int score[4];
+int tab1[64];
+int resultat[64];
+int tab3[64];
+
+void Callback_Systick(void){
+	Start_DMA1(64);
+	Wait_On_End_Of_DMA1();
+  Stop_DMA1;
+	for ( int i=0; i<64 ;i++){
+		resultat[i]=THEDFT(dma_buf, i);
+	}	
+	if(resultat[17] >=0x56a8c) {
+		compt[0]++;
+		if(compt[0] ==20){
+			score[0]++;
+			compt[0]=0;
+			StartSon();
+		}
+	}
+	if (resultat[18] >=0x56a8c){
+		compt[1]++;
+		if(compt[1] ==20){
+			score[1]++;
+			compt[1]=0;
+			StartSon();
+    }
+  }
+	if (resultat[19] >=0x56a8c){
+		compt[2]++;
+		if(compt[2] ==20){
+			score[2]++;
+			compt[2]=0;
+			StartSon();
+    }
+  }
+	if (resultat[20] >=0x56a8c){
+		compt[3]++;
+		if(compt[3] ==20){
+			score[3]++;
+			compt[3]=0;
+			StartSon();
+    }
+  }
+}
+	
 
 int main(void)
 {
@@ -17,17 +73,30 @@ int main(void)
 
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
+	for(int i=0;i<4;i++){
+		score[i]=0;
+	
+//config SysTick 
+Systick_Period_ff(360000);
+Systick_Prio_IT(10, Callback_Systick);
+SysTick_On ;
+	
+//config ADC	
+Init_TimingADC_ActiveADC_ff( ADC1, 72 );
+Single_Channel_ADC( ADC1, 2 );
+Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
+	
+//config DMA	
+Init_ADC1_DMA1( 0, dma_buf );
 
+SysTick_Enable_IT ;
 
 	
 	
 
 //============================================================================	
 	
-for(int k=0;k<64;k++){
 
-	resultat[k]=THEDFT(LeSignal,k);
-}
 while	(1)
 	{
 	}
